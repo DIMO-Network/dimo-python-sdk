02f4bc7a96d5f2e50890fb9ef93ed37eba4e0886
Barrett <barrettkowalsky@gmail.com>
2025-04-01 13:13:09 -0600

    webhook methods updated

--- dimo/api/vehicle_events.py
+++ dimo/api/vehicle_events.py
@@ -1,3 +1,4 @@
+from dimo.errors import check_type
 
 
 class VehicleEvents:
@@ -5,30 +6,82 @@ class VehicleEvents:
     def __init__(self, request_method, get_auth_headers):
         self._request = request_method
         self._get_auth_headers = get_auth_headers
-        
-    def list_webhooks(self, developer_jwt: str):
-        pass
 
-    def register_webhook(self, developer_jwt: str, request: str):
-        pass
+    def list_webhooks(self, developer_jwt: str):
+        check_type("developer_jwt", developer_jwt, str)
+        url = f"/webhooks"
+        return self._request(
+            "GET", "VehicleEvents", url, headers=self._get_auth_headers(developer_jwt)
+        )
+
+    def register_webhook(self, developer_jwt: str, request: object):
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("request", request, object)
+        url = f"/webhooks"
+        return self._request(
+            "POST",
+            "VehicleEvents",
+            url,
+            headers=self._get_auth_headers(developer_jwt),
+            json={"request": request},
+        )
 
     def webhook_signals(self, developer_jwt: str):
-        pass
+        check_type("developer_jwt", developer_jwt, str)
+        url = f"/webhooks/signals"
+        return self._request(
+            "GET", "VehicleEvents", url, headers=self._get_auth_headers(developer_jwt)
+        )
 
-    def update_webhook(self, developer_jwt: str, id: str, request: str):
-        pass
+    def update_webhook(self, developer_jwt: str, id: str, request: object):
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("id", id, str)
+        check_type("request", request, object)
+        url = f"/webhooks/{id}"
+        return self._request(
+            "PUT",
+            "VehicleEvents",
+            url,
+            headers=self._get_auth_headers(developer_jwt),
+            json=request,
+        )
 
     def delete_webhook(self, developer_jwt: str, id: str):
-        pass
-    
-    def vehicle_subscriptions(self, developer_jwt: str):
-        pass
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("id", id, str)
+        url = f"/webhooks/{id}"
+        return self._request(
+            "DELETE",
+            "VehicleEvents",
+            url,
+            headers=self._get_auth_headers(developer_jwt),
+        )
+
+    def vehicle_subscriptions(self, developer_jwt: str, token_id: str):
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("token_id", token_id, str)
+        url = f"/subscriptions/{token_id}"
+        return self._request(
+            "GET", "VehicleEvents", url, headers=self._get_auth_headers(developer_jwt)
+        )
 
     def subscribe_vehicle(self, developer_jwt: str, token_id: str, event_id: str):
-        pass
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("token_id", token_id, str)
+        check_type("event_id", event_id, str)
+        url = f"/subscriptions/{token_id}/event/{event_id}"
+        return self._request(
+            "POST", "VehicleEvents", url, headers=self._get_auth_headers(developer_jwt)
+        )
 
     def unsubscribe_vehicle(self, developer_jwt: str, token_id: str, event_id: str):
-        pass
-    
-    
-    
+        check_type("developer_jwt", developer_jwt, str)
+        check_type("token_id", token_id, str)
+        check_type("event_id", event_id, str)
+        url = f"/subscriptions/{token_id}/event/{event_id}"
+        return self._request(
+            "DELETE",
+            "VehicleEvents",
+            url,
+            headers=self._get_auth_headers(developer_jwt),
+        )
